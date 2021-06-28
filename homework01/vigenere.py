137e1a85513fb302027957833f8d47a67b424f37
RFRsk <rfrsk@yandex.ru>
2021-06-28 23:41:40 +0300

    Закончены тесты encrypt_vigenere.

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -11,6 +11,40 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    import string
+    str1 = string.ascii_uppercase
+    str2 = string.ascii_lowercase
+    delta = len(plaintext) - len(keyword)
+
+    new_keyword = ""
+    k = 0
+    if delta != 0:
+        for i in range(0, len(plaintext)):
+            if len(keyword) == 1:
+                k = 0
+            if k > len(keyword)-1:
+                k = 0
+            new_keyword += keyword[k]
+            k += 1
+    else:
+        new_keyword = keyword
+    for i in range(0, len(plaintext)):
+        if plaintext[i] in str1:
+            index = int(str1.index(plaintext[i])) + int(str1.index(new_keyword[i]))
+        else:
+            index = int(str2.index(plaintext[i])) + int(str2.index(new_keyword[i]))
+        if index > len(str1):
+            index = index - len(str1)
+            if plaintext[i] in str1:
+                ciphertext += str1[index]
+            else:
+                ciphertext += str2[index]
+            continue
+        if plaintext[i] in str1:
+            ciphertext += str1[index]
+        else:
+            ciphertext += str2[index]
+
     return ciphertext
 
 
